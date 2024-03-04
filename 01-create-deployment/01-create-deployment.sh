858e19054507cb408aca90412b3548450fc7b711
Kevin Abrar Khansa <kevariable@gmail.com>
2024-03-04 15:35:14 +0700

    Create script to expose outside world

--- 01-create-deployment/01-create-deployment.sh
+++ 01-create-deployment/01-create-deployment.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 set -eux
 
-IMAGE=kubelearn/first-app
+IMAGE=${IMAGE:-kevariable/first-app}
 
 docker build -t ${IMAGE} -f kub-action-01-starting-setup/Dockerfile ./kub-action-01-starting-setup
 
+docker push ${IMAGE}
+
+# Create Deployment
 kubectl create deployment first-app --image=${IMAGE}
 
-kubectl get deployments
+# Create Service
+kubectl expose deployment first-app --type=LoadBalancer --port=8080
 
-kubectl get deployments
-
-docker rmi ${IMAGE}
+# Hit outside world
+minikube service first-app
